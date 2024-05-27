f381046916fd420b5dae75ba61351e8bcc3acbd0
sns-chaijinrui <chai0304@163.com>
2024-05-27 19:01:00 +0800

    第一遍提交

---
+++ src/main/java/servers/GlobalSettings.java
@@ -0,0 +1,15 @@
+package servers;
+
+import utils.GetToken;
+
+public class GlobalSettings {
+    //    https://tongyi.aliyusn.com/qianwen/share?shareId=977da3f7-5810-47b7-bba3-23d42c5df586\
+
+    public void addSettings(String packageChineseName,String settingName,String settingValue){
+        //    获取token
+        String token = GetToken.token;
+    }
+
+
+}
+
---
+++ src/main/java/utils/GetToken.java
@@ -0,0 +1,53 @@
+package utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+/*
+ * 从登录接口获取token，用于其他方法的使用
+ */
+
+public class GetToken {
+    private static final String urlStr = "http://cms.cyngame.cn:8190/login/login.action?UserName=sns_cjr&PassWord=123456";
+    private static final Logger log = LogManager.getLogger(GetToken.class);
+    public static String token;
+
+
+    public static String getToken() throws IOException {
+        // 创建URL对象
+        URL url = new URL(urlStr);
+
+        // 打开连接
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        // 设置请求方法，默认是GET
+        connection.setRequestMethod("GET");
+
+        // 发送请求
+        int responseCode = connection.getResponseCode();
+        log.info("Response Code : {}", responseCode);
+        // 获取所有响应头
+        Map<String, List<String>> headerFields = connection.getHeaderFields();
+        for (Map.Entry<String, List<String>> entry : headerFields.entrySet()) {
+            if (Objects.equals(entry.getKey(), "Set-Cookie")) {
+                token = entry.getValue().get(0); // 获取第一个值
+                break;
+            }
+        }
+
+        // 关闭连接
+        connection.disconnect();
+
+//        从header中拿到JSESSIONID
+        token = "JSESSIONID=" + token.substring(token.indexOf("=") + 1, token.indexOf(";"));
+        log.info("token: {}", token);
+        return token;
+    }
+}
