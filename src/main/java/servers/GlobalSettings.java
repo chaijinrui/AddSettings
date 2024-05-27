be664cc634e86ec35749dd94539b68d954ff16a6
sns-chaijinrui <chai0304@163.com>
2024-05-27 19:03:17 +0800

    更改注释

--- src/main/java/servers/GlobalSettings.java
+++ src/main/java/servers/GlobalSettings.java
@@ -5,6 +5,8 @@ import utils.GetToken;
 public class GlobalSettings {
     //    https://tongyi.aliyusn.com/qianwen/share?shareId=977da3f7-5810-47b7-bba3-23d42c5df586\
 
+
+//    传入的报名不止一个
     public void addSettings(String packageChineseName,String settingName,String settingValue){
         //    获取token
         String token = GetToken.token;
